216c97db6248b69e80b0b51c6f5f258303699724
Danjali <anjalideoasarkar@gmail.com>
2019-01-30 19:45:45 +0530

    adding js files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import square  from './function';
 import person  from './const';
-import { test, sum } from './named_export';
+import { results, temp } from './named_export';
 
-alert(person.name);
-alert(square(4));
-alert(test.name);
-alert(sum(1, 4));
+
+console.log(results);
+
+const evaluateResult = (results) =>{
+  results.forEach(item => {
+      let temp = item.marks;
+        if(temp.english < 35 || temp.hindi < 35 || temp.mathematics <35){
+            item.result = 'fail';
+        }else{
+            item.result = 'pass';
+        }
+        results.map(item => item.result);
+  })
+};
+
+const  calculatePercentage = (results) =>{
+  results.forEach(item =>{
+    let temp = item.marks;
+    item.percentage = ((temp.english + temp.hindi + temp.mathematics) / 300)*100;
+    results.map(item => item.percentage);
+  })
+};
+
+const  calculateTotalMarks = (results) =>{
+  results.forEach(item =>{
+    let temp = item.marks;
+    item.totalMarks = temp.english + temp.hindi + temp.mathematics;
+    results.map(item => item.totalMarks);
+  })
+};
--- src/named_export.js
+++ src/named_export.js
@@ -1,4 +1,20 @@
-export const test = { name: 'test'};
-export function sum(a, b) {
-    return a +  b;
+class Studentresults {
+  connstructor(name, lname, marks){
+    this._name = name;
+    this._name = name;
+    this._name = name;
+  }
 }
+
+const results = [
+  {"firstName": "Sanket", "lastName":"Gadade","marks":{"english":70, "hindi":60,"mathematics":46}},
+  {"firstName": "Shubham", "lastName":"Laad","marks":{"english":40, "hindi":55,"mathematics":46}},
+  {"firstName": "Swapnil", "lastName":"Patil","marks":{"english":50, "hindi":60,"mathematics":77}},
+  {"firstName": "Ankita", "lastName":"Pawar","marks":{"english":50, "hindi":55,"mathematics":46}},
+  {"firstName": "Vijayraj", "lastName":"Nathe","marks":{"english":35, "hindi":45,"mathematics":11}}
+];
+const temp ='anjali';
+
+
+export  {results, temp};
+// export default temp;
